463ad9bb6eeba06b973c665e215cea50be68b9bb
pouriahassani <poria19964@gmail.com>
2024-06-16 22:47:46 +0200

    cretae environment

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update package lists and install Python 3.10 and development headers
+apt update
+apt install -y python3.10 python3.10-dev
+
+# Install Python pip
+apt-get install -y python3-pip
+
+# Install Python packages using pip
+pip install six
+pip install psutil
+
+# Source the upmem_env.sh script (assuming it's in the current directory)
+source upmem_env.sh
+
+# Install pkg-config
+apt install -y pkg-config
