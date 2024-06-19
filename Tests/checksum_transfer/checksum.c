620985051308df59a8c8c849030e54783c58df35
pouriahassani <poria19964@gmail.com>
2024-06-19 09:25:53 +0200

    New tests

---
+++ Tests/checksum_transfer/checksum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <mram.h>
+#include <stdint.h>
+#define BUFER_SIZE 1024
+#define NR_ELEMENTS_PER_TASKLET (BUFER_SIZE / NR_TASKLETS)
+__mram uint32_t buffer[BUFFER_SIZE];
+uint32_t checksums[NR_TASKLTES] = {0};
+int main(){
+
+    for(int i = me()*NR_ELEMENTS_PER_TASKLET ;i < (me()+1)*NR_ELEMENTS_PER_TASKLET;i++){
+        checksums[me()] += buffer[i];
+    }
+    if(!me()){
+        uint32_t checksum =0;
+        for(int i = 0;i<NR_TASKLETS;i++){
+            checksum += checksums[i];
+        }
+        printf("\nThe checksum is %u",checksum);
+    }
+}
